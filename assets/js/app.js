11b9c6f336c3d30e272c2e9fcdba48200b25f3ab
Rob Martin <rob.martin.5758@gmail.com>
2018-03-31 18:01:21 -0400

    get and display gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,60 @@
 const giphyAPIKey = '5J2DxEwoOGML6C2M2ZCBNbziJTSHovI1';
-const initialButtonArr = ['UNC', 'Duke', 'NC State', 'Wake Forest', 'ECU', 'UNCG'];
-
-
-
-
-// function newButton(text) {
-
-//     let button = '<button type="button" class="btn">' + text + '</button>'
-
-//     var a = $("<button>");
-//           // Adds a class of movie to our button
-//           a.addClass("");
-//           // Added a data-attribute
-//           a.attr("data-name", movies[i]);
-//           // Provided the initial button text
-//           a.text(movies[i]);
-//           // Added the button to the buttons-view div
-//           $("#buttons-view").append(a);
-
-// $("#animalButtons").append(button)
-//     return;
-
-// };
-// newButton("aa");
-// newButton("bb");
+const initialButtonArr = ['UNC', 'Duke', 'NC State', 'Wake Forest', 'ECU', 'UNCG', 'UNLV'];
+const theme = 'mascot';
 
 function displayButtons() {
 
     for (let i = 0; i < initialButtonArr.length; i++) {
-
-        let newBtn = $('button');
+        let newBtn = $('<button>');
         newBtn.text(initialButtonArr[i]);
-        newBtn.addClass('list');
-        $('#animalButtons').append(newBtn);
-    
+        newBtn.attr('data-value', initialButtonArr[i]);
+        newBtn.addClass('listBtn btn');
+        $('#gifButtons').append(newBtn);
     };
 };
+$(document).ready(function () {
+    $('.listBtn').on('click', function () {
+        
+        let selectedItem = $(this).attr('data-value');
+        let searchTerm = selectedItem + ' ' + theme;
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            searchTerm + "&api_key=" + giphyAPIKey + "&limit=10";
+            
+            $("#gifs").empty();
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                let results = response.data;
+                console.log(results);
+
+                for (var i = 0; i < results.length; i++) {
+                
+                    var gifDiv = $("<div class='gif'>");
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var gifImg = $("<img>");
+                    gifImg.attr("src", results[i].images.fixed_height.url);
+                    // gifImg.text("Rating: " + rating);
+
+                    gifDiv.append(p);
+                    gifDiv.append(gifImg);
+
+                    $("#gifs").append(gifDiv);
+
+                  
+                }
+               
+            });
+    });
+});
 
 
-
-function getGif(e) {
-
-    let searchTerm = $(this).text;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        searchTerm + "&api_key=" + giphyAPIKey + "&limit=10";
-
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            let results = response.data;
-
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div class='item'>");
-
-                var rating = results[i].rating;
-
-                var p = $("<p>").text("Rating: " + rating);
-
-                var personImage = $("<img>");
-                personImage.attr("src", results[i].images.fixed_height.url);
-
-                gifDiv.prepend(p);
-                gifDiv.prepend(personImage);
-
-                $("#animalGifs").prepend(gifDiv);
-            }
-        });
-};
-
-
-$(document).on('click', '#list', getGif );
+// $(document).on('click', '#list', getGif);
 
 displayButtons();
 // });
