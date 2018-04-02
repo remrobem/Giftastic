d2da5e60760f2f1b9e8f2af27e6ee46bdfc96de0
Rob Martin <rob.martin.5758@gmail.com>
2018-04-02 14:34:37 -0400

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const giphyAPIKey = '5J2DxEwoOGML6C2M2ZCBNbziJTSHovI1';
-const theme = 'mascot';
+const theme = 'mascots';
 
-let buttonArr = ['UNC', 'Duke', 'NC State', 'Wake Forest', 'ECU', 'UNCG', 'UNLV'];
+let buttonArr = ['MLB', 'NBA', 'NHL', 'NFL', 'RACING', 'EATING', 'DANCING', 'KIDS'];
 
 function displayButtons() {
 
@@ -11,66 +11,106 @@ function displayButtons() {
         let newBtn = $('<button>');
         newBtn.text(buttonArr[i]);
         newBtn.attr('data-value', buttonArr[i]);
+        newBtn.attr('data-index', i);
         newBtn.addClass('listBtn btn');
         $('#gifButtons').append(newBtn);
     };
 };
 
-// $('.listBtn').on('click', function () {
-
 function displayGifs() {
 
     let selectedItem = $(this).attr('data-value');
-    let searchTerm = selectedItem + ' ' + theme;
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        searchTerm + "&api_key=" + giphyAPIKey + "&limit=10";
+    let searchTerm = theme + ' ' + selectedItem;
+    searchTerm = searchTerm.replace(/ /g, '+');
+    console.log(searchTerm);
+    let queryURL = 'https://api.giphy.com/v1/gifs/search?q=' +
+        searchTerm + '&api_key=' + giphyAPIKey + '&limit=15';
 
-    $("#gifs").empty();
+    $('#gifs').empty();
 
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: 'GET'
     }).then(function (response) {
-        let results = response.data;       
+        let results = response.data;
         console.log(results);
 
         for (var i = 0; i < results.length; i++) {
-            if (['g', 'pg-13'].includes(results[i].rating)) {
-                var gifDiv = $("<div class='gif'>");
+            if (['g', 'pg', 'pg-13'].includes(results[i].rating)) {
+                
                 var rating = results[i].rating;
-
-                var p = $("<p>").text("Rating: " + rating);
-
-                var gifImg = $("<img>");
-                gifImg.attr("src", results[i].images.fixed_height.url);
-                // gifImg.text("Rating: " + rating);
-
+                var gifImg = $('<img>');
+                var gifDiv = $('<div></div>');
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                
+                gifDiv.attr('class', 'floatBox');                
+                gifImg.attr('class', 'gif');
+                gifImg.attr('src', results[i].images.fixed_height_still.url);
+                gifImg.attr('data-state', 'still');
+                gifImg.attr('data-still', results[i].images.fixed_height_still.url);
+                gifImg.attr('data-animate', results[i].images.fixed_height.url);          
+                         
                 gifDiv.append(p);
                 gifDiv.append(gifImg);
+                $('#gifs').prepend(gifDiv);
 
-                $("#gifs").append(gifDiv);
+
+                
+              
             };
         };
     });
 };
 
-// });
+function animation() {
 
-$("#addList").on("click", function (event) {
+    var state = $(this).attr('data-state');
+    console.log(state);
+    if (state === 'still') {
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state', 'still');
+    };
+};
+
+function addButton(event) {
+
     event.preventDefault();
-    let item = $("#gifInput").val().trim();
+    item = $('#gifInput').val().trim();
+    item = item.toUpperCase();
     buttonArr.push(item);
-    displayButtons();
-});
+    // Set creates a new object with unique values from the buttonArr but it is not an array
+    // Using [...] converts it into an array
+    uniqueArr = [...(new Set(buttonArr))];
+    // if the unique array is less than the original button array, then user attempted to existing value
+    if (uniqueArr.length !== buttonArr.length) {
+        buttonArr = uniqueArr;
+        alert(`Entry ${item} requested already exists`);
+    } else {
+        // clear the usique array
+        uniqueArr = [];
+        displayButtons();
+        buttonAdded = (buttonArr.length - 1).toString();
+        //simulate click of the new button
+        $('.listBtn[data-index="' + buttonAdded + '"]').trigger('click');
+    };
 
-// $('.listBtn').on('click', function () {
+    $('#gifInput').val('');
 
-$(document).on('click', '.listBtn', displayGifs);
+
+};
+
 
 
 $(document).ready(function () {
 
     displayButtons();
 
+    $(document).on('click', '.listBtn', displayGifs);
+
+    $(document).on('click', '.gif', animation); 
+
+    $(document).on('click', '#addButton', addButton); 
 });
-// });
